ed9c98496e54f13c80cc69950e95bbc92251e954
Max Boyko <maxboykomax@gmail.com>
2020-05-22 12:07:34 -0700

    feat: Add a trivia service to the docker-compose.yml

--- services/trivia/project/__init__.py
+++ services/trivia/project/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+import os
 
 # instantiate app
 app = Flask(__name__)
@@ -7,13 +8,13 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv("APP_SETTINGS")
+app.config.from_object("project.config.DevelopmentConfig")
+
 
 class Ping(Resource):
     def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+        return {"status": "success", "message": "pong!"}
 
-api.add_resource(Ping, '/ping')
+
+api.add_resource(Ping, "/ping")
